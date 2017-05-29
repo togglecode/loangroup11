83007abb3c9e8ea893abeb823a94b784f4ad7b39
payam <payam.foroushan@huemer-dc.com>
2017-05-29 12:06:16 +0200

    init

---
+++ src/main/java/org/camunda/bpm/application/MyProcess.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.application;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class MyProcess extends ServletProcessApplication {
+  // empty implementation
+}
